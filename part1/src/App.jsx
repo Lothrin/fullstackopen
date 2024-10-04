8db31aa6e093bdcb004d7a888c8aeb624eff7ed5
Lothrin <m.erkam.arafat@gmail.com>
2024-10-04 11:52:46 +0200

    1.2: Course Information, step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,12 +6,22 @@ const Header = (course) => {
   )
 }
 
-const Content = (exercise) => {
+const Part = (exercise) => {
   return (
     <>
       <p>
         {exercise.name} = {exercise.value}
       </p>
+    </>
+  )
+}
+
+const Content = () => {
+  return (
+    <>
+      <Part name= 'Fundamentals of React' value={10}/>
+      <Part name= 'Using props to pass data' value={7}/>
+      <Part name= 'State of a component' value={14}/>
     </>
     )
 }
@@ -27,19 +37,15 @@ const Total = (value) => {
 
 const App = () => {
   const courseName = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header name={courseName}/>
-      <Content name={part1} value={exercises1}/>
-      <Content name={part2} value={exercises2}/>
-      <Content name={part3} value={exercises3}/>
+      <Content/>
       <Total first={exercises1} second={exercises2} third={exercises3} />
     </div>
   )
