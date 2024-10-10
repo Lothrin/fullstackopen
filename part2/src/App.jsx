ee2588c01c4afe9b3f3d9de88699b02a1e2c9ca2
Lothrin <m.erkam.arafat@gmail.com>
2024-10-10 13:40:01 +0200

    2.11: The Phonebook Step 6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import  Filter  from './components/filter'
 import PersonForm from './components/form'
 import PersonList from './components/person_list'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newPerson, setNewPerson] = useState('')
   const [newPersonNumber, setNewPersonNumber] = useState('')
-
   const [searchPersons, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3002/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
 
   const filterPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchPersons.toLowerCase())
